4478da647a99c2b8a92bc6fb5325646cceb657e5
Owen <owenyangg@gmail.com>
2016-03-30 09:26:33 -1000

    completed matrix generator

---
+++ matrix-generator.js
@@ -0,0 +1,28 @@
+function generator (rows, columns){
+  var myArr = [];
+
+  for (var n = 0; n < rows * columns; n++){
+    myArr.push(Math.floor(Math.random() * 100) + 1);
+  }
+
+
+  myArr.sort(function(a,b){
+    return a - b;
+  });
+
+  var myMatrix = [];
+
+  var index = 0;
+
+  for (var c = 0; c < columns; c++){
+    myMatrix.push([]);
+    for (var r = 0; r < rows; r++){
+      myMatrix[c].push(myArr[index]);
+      index++;
+    }
+  }
+
+  return myMatrix;
+}
+
+console.log(generator(3,3));
